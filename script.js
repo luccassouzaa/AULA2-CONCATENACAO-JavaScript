b5587920405a88d7f279c72587500c7da7dc87bc
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-13 18:27:15 -0300

    v1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+//CONCATENAÇÃO
+
+// Simples
+
+let produto = 'camiseta'
+let preco = 10
+
+console.log("O preço da " + produto + " é " + preco + " reais")
+
+// Composto
+
+let produtos = ["camiseta", "camisa", "calça", "shorts", "tenis"]
+let precos = [10, 12, 14, 20, 90]
+
+console.log("O preço do(a) " + produtos[3] + " é " + precos[3] + " reais")
